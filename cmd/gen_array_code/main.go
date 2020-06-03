9def527a05a207bc46c31a2c6c819f54022351af
electricface <songwentai@deepin.com>
2020-06-03 22:38:37 +0800

    gi: add array types

---
+++ cmd/gen_array_code/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+const templateTxt = `
+type {{ .TypeName }}Array struct {
+	P unsafe.Pointer
+	Len int
+}
+
+func New{{ .TypeName }}Array(values ...{{ .GoElemType }}) {{ .TypeName }}Array {
+	size := int(unsafe.Sizeof({{ .GoElemType }}(0))) * len(values)
+	p := Malloc(size)
+	arr := {{ .TypeName }}Array{
+		P:   p,
+		Len: len(values),
+	}
+	slice := arr.AsSlice()
+	copy(slice, values)
+	return arr
+}
+
+func (arr {{ .TypeName }}Array) Free() {
+	Free(arr.P)
+	arr.P = nil
+}
+
+func (arr {{ .TypeName }}Array) AsSlice() []{{ .GoElemType }} {
+	if arr.Len < 0 {
+		panic("arr.len < 0")
+	}
+	slice := (*(*[1 << 31]{{ .GoElemType }})(arr.P))[:arr.Len:arr.Len]
+	return slice
+}
+
+func (arr {{ .TypeName }}Array) Copy() []{{ .GoElemType }} {
+	if arr.Len < 0 {
+		panic("arr.len < 0")
+	}
+	if arr.Len == 0 {
+		return nil
+	}
+	result := make([]{{ .GoElemType }}, arr.Len)
+	slice := (*(*[1 << 32]{{ .GoElemType }})(arr.P))[:arr.Len:arr.Len]
+	copy(result, slice)
+	return result
+}
+
+`
+
+type params struct {
+	TypeName   string
+	GoElemType string
+}
+
+func main() {
+	t1 := template.New("t1")
+	_, err := t1.Parse(templateTxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	arrParams := []params{
+		{TypeName: "Double", GoElemType: "float64"},
+		{TypeName: "Float", GoElemType: "float32"},
+		{TypeName: "UniChar", GoElemType: "rune"},
+		{TypeName: "Int8", GoElemType: "int8"},
+		{TypeName: "Uint8", GoElemType: "uint8"},
+		{TypeName: "Int16", GoElemType: "int16"},
+		{TypeName: "Uint16", GoElemType: "uint16"},
+		{TypeName: "Int32", GoElemType: "int32"},
+		{TypeName: "Uint32", GoElemType: "uint32"},
+		{TypeName: "Int64", GoElemType: "int64"},
+		{TypeName: "Uint64", GoElemType: "uint64"},
+	}
+	for _, param := range arrParams {
+		err = t1.Execute(os.Stdout, param)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
---
+++ gi/arg.go
@@ -0,0 +1,205 @@
+package gi
+
+/*
+#include <girepository.h>
+ */
+import "C"
+import "unsafe"
+
+// C.GIArgument is [8]byte
+type Argument [8]byte
+
+func NewUint8Argument(v uint8) (arg Argument) {
+	*(*uint8)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewInt8Argument(v int8) (arg Argument) {
+	*(*int8)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewBoolArgument(v bool) (arg Argument) {
+	var val C.gboolean
+	if v {
+		val = 1
+	}
+	*(*C.gboolean)(unsafe.Pointer(&arg)) = val
+	return
+}
+
+func NewUint16Argument(v uint16) (arg Argument) {
+	*(*uint16)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewInt16Argument(v int16) (arg Argument) {
+	*(*int16)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewUint32Argument(v uint32) (arg Argument) {
+	*(*uint32)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewInt32Argument(v int32) (arg Argument) {
+	*(*int32)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewUint64Argument(v uint64) (arg Argument) {
+	*(*uint64)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewInt64Argument(v int64) (arg Argument) {
+	*(*int64)(unsafe.Pointer(&arg)) = v
+	return
+}
+
+func NewFloatArgument(v float32) (arg Argument) {
+	*(*C.gfloat)(unsafe.Pointer(&arg)) = (C.gfloat)(v)
+	return
+}
+
+func NewDoubleArgument(v float64) (arg Argument) {
+	*(*C.gdouble)(unsafe.Pointer(&arg)) = (C.gdouble)(v)
+	return
+}
+
+func NewShortArgument(v int16) (arg Argument) {
+	*(*C.gshort)(unsafe.Pointer(&arg)) = (C.gshort)(v)
+	return
+}
+
+func NewUShortArgument(v int16) (arg Argument) {
+	*(*C.gushort)(unsafe.Pointer(&arg)) = (C.gushort)(v)
+	return
+}
+
+func NewIntArgument(v int) (arg Argument) {
+	*(*C.gint)(unsafe.Pointer(&arg)) = (C.gint)(v)
+	return
+}
+
+func NewUintArgument(v uint) (arg Argument) {
+	*(*C.guint)(unsafe.Pointer(&arg)) = (C.guint)(v)
+	return
+}
+
+func NewLongArgument(v int64) (arg Argument) {
+	*(*C.glong)(unsafe.Pointer(&arg)) = (C.glong)(v)
+	return
+}
+
+func NewULongArgument(v uint64) (arg Argument) {
+	*(*C.gulong)(unsafe.Pointer(&arg)) = (C.gulong)(v)
+	return
+}
+
+func NewSSizeArgument(v int64) (arg Argument) {
+	*(*C.gssize)(unsafe.Pointer(&arg)) = (C.gssize)(v)
+	return
+}
+
+func NewSizeArgument(v uint64) (arg Argument) {
+	*(*C.gsize)(unsafe.Pointer(&arg)) = (C.gsize)(v)
+	return
+}
+
+func NewStringArgument(v unsafe.Pointer) (arg Argument) {
+	*(**C.gchar)(unsafe.Pointer(&arg)) = (*C.gchar)(v)
+	return
+}
+
+func NewPointerArgument(v unsafe.Pointer) (arg Argument) {
+	*(*C.gpointer)(unsafe.Pointer(&arg)) = (C.gpointer)(v)
+	return
+}
+
+func (arg Argument) Bool() bool {
+	val := *(*C.gboolean)(unsafe.Pointer(&arg))
+	return val != 0
+}
+
+func (arg Argument) Int8() int8 {
+	return *(*int8)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Uint8() uint8 {
+	return *(*uint8)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Int16() int16 {
+	return *(*int16)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Uint16() uint16 {
+	return *(*uint16)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Int32() int32 {
+	return *(*int32)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Uint32() uint32 {
+	return *(*uint32)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Int64() int64 {
+	return *(*int64)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Uint64() uint64 {
+	return *(*uint64)(unsafe.Pointer(&arg))
+}
+
+func (arg Argument) Float() float32 {
+	return float32(*(*C.gfloat)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) Double() float64 {
+	return float64(*(*C.gdouble)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) Short() int16 {
+	return int16(*(*C.gshort)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) UShort() uint16 {
+	return uint16(*(*C.gushort)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) Int() int {
+	return int(*(*C.gint)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) Uint() uint {
+	return uint(*(*C.guint)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) Long() int64 {
+	return int64(*(*C.glong)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) ULong() uint64 {
+	return uint64(*(*C.gulong)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) SSize() int64 {
+	return int64(*(*C.gssize)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) Size() uint64 {
+	return uint64(*(*C.gsize)(unsafe.Pointer(&arg)))
+}
+
+func (arg Argument) String() strPtr {
+	return strPtr{*(*unsafe.Pointer)(unsafe.Pointer(&arg))}
+}
+
+func (arg Argument) Pointer() unsafe.Pointer {
+	return *(*unsafe.Pointer)(unsafe.Pointer(&arg))
+}
+
